6f3a19af37e3db22fff3c00cced88a99e3d0a377
Rayan Khan <rayankhannexus@gmail.com>
2026-03-15 14:22:09 +0500

    constants + util class

---
+++ src/main/java/com/example/backend/services/database/Constants.java
@@ -0,0 +1,16 @@
+package com.example.backend.services.database;
+
+public class Constants 
+{
+    public static final String Id = "Id";
+    public static final String First_Name = "first_name";
+    public static final String LAST_Name = "last_name";
+    public static final String Email = "email";
+    public static final String Password = "password";
+    public static final String Title = "title";
+    public static final String Status = "status";
+    public static final String Priority = "priority";
+    public static final String Due_Date = "due_date";
+    public static final String Created_Date = "created_date";
+    public static final String Description = "description";
+}
---
+++ src/main/java/com/example/backend/util/Util.java
@@ -0,0 +1,23 @@
+package com.example.backend.util;
+
+import java.sql.ResultSet;
+import java.util.Optional;
+
+public class Util 
+{
+    public static <T> Optional<T> MapResultToModel(ResultSet result, Class<T> type) 
+    {
+        try 
+        {
+            var constructor = type.getConstructor(ResultSet.class);
+            if (result.next())
+                return Optional.of(constructor.newInstance(result));
+        } 
+        catch (Exception e) 
+        {
+            return Optional.empty();
+        }
+        
+        return Optional.empty();
+    }    
+}
