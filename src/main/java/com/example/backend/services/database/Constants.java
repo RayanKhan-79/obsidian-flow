a014ad3088c7f261fae88c383cc41f8f5755562e
Rayan Khan <rayankhannexus@gmail.com>
2026-03-16 02:33:38 +0500

    Project Entity

--- src/main/java/com/example/backend/services/database/Constants.java
+++ src/main/java/com/example/backend/services/database/Constants.java
@@ -2,10 +2,10 @@ package com.example.backend.services.database;
 
 public class Constants 
 {
-    public static final String Id = "Id";
-    public static final String First_Name = "first_name";
-    public static final String LAST_Name = "last_name";
-    public static final String Email = "email";
+    public static final String ID = "Id";
+    public static final String FIRST_NAME = "first_name";
+    public static final String LAST_NAME = "last_name";
+    public static final String EMAIL = "email";
     public static final String Password = "password";
     public static final String Title = "title";
     public static final String Status = "status";
@@ -13,4 +13,7 @@ public class Constants
     public static final String Due_Date = "due_date";
     public static final String Created_Date = "created_date";
     public static final String Description = "description";
+    public static final String USER_ID = "user_id";
+    public static final String PROJECT_ID = "project_id";
+    public static final String Manager_Id = "manager_id";
 }
