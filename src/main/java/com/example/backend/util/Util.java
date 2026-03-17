e9b6188cacdbf18dde03ba4cd8b407290403e26b
Rayan Khan <rayankhannexus@gmail.com>
2026-03-17 23:17:38 +0500

    list mapper + facade controller (temporary fix)

--- src/main/java/com/example/backend/util/Util.java
+++ src/main/java/com/example/backend/util/Util.java
@@ -1,6 +1,8 @@
 package com.example.backend.util;
 
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class Util 
@@ -17,5 +19,19 @@ public class Util
         }
         
         return Optional.empty();
-    }    
+    }
+    
+    public static <T> List<T> MapResultToModelList(ResultSet result, Class<T> type)
+    {
+        ArrayList<T> mapped = new ArrayList<>();
+        try 
+        {
+            var constructor = type.getConstructor(ResultSet.class);
+            while (result.next())
+                mapped.add(constructor.newInstance(result));
+        } catch (Exception e) {
+            
+        }
+        return mapped;
+    }
 }
